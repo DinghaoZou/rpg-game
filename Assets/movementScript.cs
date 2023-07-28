9f20130c9c57aec1117714c91f84e7bbcd1042ba
DinghaoZou <dinghaozou0@gmail.com>
2023-07-27 22:41:36 -0700

    a

--- Assets/movementScript.cs
+++ Assets/movementScript.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class movementScript : MonoBehaviour
 {
+    [SerializeField] float moveSpeed;
+    float vertical, horizontal;
+    Rigidbody2D thePlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        thePlayer = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * 15f * Time.deltaTime, 0f, 0f);
-        transform.Translate(Input.GetAxis("Vertical") * 15f * Time.deltaTime, 0f, 0f);
+       MovePlayer();
+    }
+
+    void MovePlayer() {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        thePlayer.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
     }
 }
